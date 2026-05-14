e549572b6a25b1881a55d2579129184b589ebf92
dj <dajcordova@gmail.com>
2026-05-14 08:51:49 -0600

    :art: order the config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http'; // <-- Asegúrate de incluir withFetch aquí
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { registerLocaleData } from '@angular/common';
-import localeEsSV from '@angular/common/locales/es-SV'; // Locale específico de El Salvador
+import localeEsSV from '@angular/common/locales/es-SV';
 registerLocaleData(localeEsSV);
 
 
@@ -22,18 +22,12 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: false // <--- ¡Esto apaga el modo oscuro automático!
+          darkModeSelector: false
         }
       }
     })
   ]
 };
 
-
-
-
-// En tus Providers:
-providers: [
-]
 
 
