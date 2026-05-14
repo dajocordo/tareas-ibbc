0b93df0a18746819ff4ee4c4f9085de4c285346d
dj <dajcordova@gmail.com>
2026-05-14 08:45:54 -0600

    :globe_with_meridians: update local timezone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http'; // <-- Asegúrate de incluir withFetch aquí
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { registerLocaleData } from '@angular/common';
+import localeEsSV from '@angular/common/locales/es-SV'; // Locale específico de El Salvador
+registerLocaleData(localeEsSV);
 
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'es-SV' },
+    provideHttpClient(withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
@@ -22,5 +28,12 @@ export const appConfig: ApplicationConfig = {
     })
   ]
 };
+
+
+
+
+// En tus Providers:
+providers: [
+]
 
 
